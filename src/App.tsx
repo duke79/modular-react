a71676da88afe6b7d30ba4b9f3aabea40320a11c
Pulkit Singh Baliyan <pulkitsingh01@gmail.com>
2023-12-19 21:25:18 +0000

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,38 @@
 import { useState } from "react";
 import { modules } from "./modules";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
 
 function App() {
-  return <div>{JSON.stringify(modules, null, 2)}</div>;
   return (
     <Router>
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route path="/about" component={About} />
-        <Route path="/contact" component={Contact} />
-      </Switch>
+      <Routes>
+        <Route
+          exact
+          path="/"
+          element={
+            <div>
+              <h1>Routes</h1>
+              {modules.map((module) => (
+                <div key={module.routes[0].path}>
+                  <a href={module.routes[0].path}>{module.routes[0].path}</a>
+                </div>
+              ))}
+            </div>
+          }
+        />
+        {modules.map((module) => (
+          <Route
+            key={module.routes[0].path}
+            path={module.routes[0].path}
+            component={module.component}
+          />
+        ))}
+      </Routes>
     </Router>
   );
 }
