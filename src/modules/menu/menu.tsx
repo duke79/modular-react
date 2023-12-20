32b60e2639e8d392ece89dce9070c91a868c7a78
Pulkit Singh Baliyan <pulkitsingh01@gmail.com>
2023-12-20 03:25:14 +0000

    feat: Updated menu and projects modules
    Added active class to menu items and implemented projects section in projects module

--- src/modules/menu/menu.tsx
+++ src/modules/menu/menu.tsx
@@ -3,23 +3,19 @@ import styles from "./menu.module.scss";
 import { Link } from "react-router-dom";
 
 export const Component = () => {
-  const items = [
-    {
-      label: "Home",
-      path: "/",
-    },
-    {
-      label: "About",
-      path: "/about",
-    },
-  ];
+  console.log(modules, window.location.pathname);
   return (
     <div className={styles.container}>
       <div className={styles.left}>
         {modules
           .filter((m) => m.route.menu)
           .map(({ route }) => (
-            <div key={route.path} className={styles.item}>
+            <div
+              key={route.path}
+              className={`${styles.item} ${
+                route.path === window.location.pathname ? styles.active : ""
+              }`}
+            >
               <Link to={route.path}>{route.menu?.label}</Link>
             </div>
           ))}
--- src/modules/projects/projects.tsx
+++ src/modules/projects/projects.tsx
@@ -2,9 +2,126 @@ import styles from "./projects.module.scss";
 import Menu from "../menu/menu";
 
 const ModuleTwo = () => {
+  const projects = [
+    {
+      label: "Care for the living",
+      summary:
+        "During the COVID times oversaw & helped building an app to help patients connect with donors",
+      codeLink: "https://github.com/carefortheliving/frontend",
+      demoLink: "http://carefortheliving.org",
+      tech: ["HTML", "CSS", "JavaScript"],
+    },
+    {
+      label: "Gaveshak",
+      summary:
+        "Code & tools in the office didn’t change frequently, hence, got interested in doing something different with C++",
+      codeLink: "https://github.com/duke79/Gaveshak",
+      tech: ["C++", "Cmake", "Boost", "Gumbo"],
+    },
+    {
+      label: "Abhidhana",
+      summary:
+        "Code & tools in the office didn’t change frequently, hence, got interested in doing something different with Flutter",
+      codeLink: "https://github.com/duke79/Abhidhana",
+      tech: ["Flutter"],
+    },
+    {
+      label: "dukepy",
+      summary:
+        "Code & tools in the office didn’t change frequently, hence, got interested in doing something different with Python",
+      codeLink: "https://github.com/duke79/dukepy",
+      tech: ["Python"],
+    },
+    {
+      label: "Samanvaya",
+      summary:
+        "Code & tools in the office didn’t change frequently, hence, got interested in doing something different with Python",
+      codeLink: "https://github.com/duke79/Samanvaya",
+      tech: ["Python"],
+    },
+    {
+      label: "pyWeb",
+      summary:
+        "Code & tools in the office didn’t change frequently, hence, got interested in doing something different with Python",
+      codeLink: "https://github.com/duke79/pyWeb",
+      tech: ["Python"],
+    },
+    {
+      label: "Kritrima",
+      summary:
+        "Code & tools in the office didn’t change frequently, hence, got interested in doing something different with Python",
+      codeLink: "https://github.com/duke79/Kritrima",
+      tech: ["Python"],
+    },
+    {
+      label: "compro",
+      summary:
+        "Code & tools in the office didn’t change frequently, hence, got interested in doing something different with Python",
+      codeLink: "https://github.com/duke79/compro",
+      tech: ["Python"],
+    },
+    {
+      label: "alligator",
+      summary:
+        "Code & tools in the office didn’t change frequently, hence, got interested in doing something different with Python",
+      codeLink: "https://github.com/duke79/alligator",
+      tech: ["Python"],
+    },
+    {
+      label: "pysync",
+      summary:
+        "Code & tools in the office didn’t change frequently, hence, got interested in doing something different with Python",
+      codeLink: "https://github.com/duke79/pysync",
+      tech: ["Python"],
+    },
+    {
+      label: "Yojaka",
+      summary:
+        "Code & tools in the office didn’t change frequently, hence, got interested in doing something different with Python",
+      codeLink: "https://github.com/duke79/Yojaka",
+      tech: ["Python"],
+    },
+  ];
   return (
-    <div className={styles.container}>
+    <div
+      className={styles.container}
+      style={{
+        // background: "linear-gradient(90deg, #FFFFFF 0%, #F8F8F8 100%)",
+        // radial gradient
+        background:
+          "radial-gradient(50% 50% at 50% 50%, rgb(254 245 213) 0%, rgb(241 224 224) 100%)",
+      }}
+    >
       <Menu />
+      <div className={styles.projects}>
+        {projects.map((project) => (
+          <div key={project.label} className={styles.project}>
+            <h3>{project.label}</h3>
+            <p className={styles.summary}>{project.summary}</p>
+            <div className={styles.links}>
+              <a target="_blank" href={project.codeLink} rel="noreferrer">
+                Code
+              </a>
+              {project.demoLink ? (
+                <a target="_blank" href={project.demoLink} rel="noreferrer">
+                  Demo
+                </a>
+              ) : null}
+            </div>
+            <div className={styles.tech}>
+              {project.tech.map((tech) => (
+                <div key={tech} className={styles.techItem}>
+                  {tech}
+                </div>
+              ))}
+            </div>
+          </div>
+        ))}
+        <div className={styles.footer} />
+        <div className={styles.footer} />
+        <div className={styles.footer} />
+        <div className={styles.footer} />
+      </div>
     </div>
   );
 };
